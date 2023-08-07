5fff4b2fec54b3710d0a148357517fcbfb0e3da1
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-08-07 22:29:23 +0500

    Catalogue: Adding Movie Class

---
+++ classes/movie/movie.rb
@@ -0,0 +1,12 @@
+require_relative '../item_movie'
+
+class Movie < Item_Movie
+  def initialize(publish_date, id: nil, silent: true)
+    super(publish_date: publish_date, id: id)
+    @silent = silent
+  end
+
+  def can_be_archived?
+    super && silent
+  end
+end
