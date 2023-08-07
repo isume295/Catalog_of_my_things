4275722098d6697a2ffafb027f29d2aae6de335f
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-07 17:32:46 +0300

    Update music_album.rb

--- classes/music/music_album.rb
+++ classes/music/music_album.rb
@@ -1,6 +1,8 @@
 require '../item'
 
 class MusicAlbum < Item
+    attr_reader :on_spotify, :publish_date
+    
   def initialize(on_spotify:, publish_date:)
     super(publish_date)
     @on_spotify = on_spotify
