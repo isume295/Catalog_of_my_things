9d2722a3b7829e751e64694511a1a8a337342334
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-07 22:55:15 +0300

    Create item_salim.rb

---
+++ classes/item_salim.rb
@@ -0,0 +1,29 @@
+require 'date'
+
+class Item
+  attr_reader :id
+  def initialize(publish_date, id = nil)
+    @id = id || Random.rand(1..1000)
+    @publish_date = publish_date
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  def add_author; end
+
+  def add_label; end
+
+  def add_source; end
+
+  def can_be_archived?
+    current_year = Date.today.year
+    publish_year = Date.parse(@publish_date).year
+    current_year - publish_year > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+end
