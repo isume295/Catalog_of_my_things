7fa931b612a49df4114a7f58fa45685726982f60
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-07 16:22:26 +0300

    Update item.rb

--- classes/item.rb
+++ classes/item.rb
@@ -1,34 +1,26 @@
 require 'date'
 
 class Item
-    def initialize(id = nil, publish_date)
-        @id = id || random.rand(1..1000) 
-        @publish_date = publish_date
-    end
+  def initialize(publish_date, id = nil)
+    @id = id || random.rand(1..1000)
+    @publish_date = publish_date
+  end
 
-    def add_genre
+  def add_genre; end
 
-    end
+  def add_author; end
 
-    def add_author
+  def add_label; end
 
-    end
+  def add_source; end
 
-    def add_label
+  def can_be_archived?
+    current_year = Date.today.year
+    publish_year = Date.parse(@publish_date).year
+    current_year - publish_year > 10
+  end
 
-    end
-
-    def add_source
-
-    end
-
-    def can_be_archived?
-        current_year = Date.today.year
-        publish_year = Date.parse(@publish_date).year
-        current_year - publish_year > 10
-    end
-
-    def move_to_archive
-        @archived = true if can_be_archived?
-    end
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 end
