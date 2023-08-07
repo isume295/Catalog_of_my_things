6adf19a95484233757d4b9eb4c71f0f1ecd8e03d
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-07 17:17:31 +0300

    Fix linters

--- classes/music/music_album.rb
+++ classes/music/music_album.rb
@@ -1,12 +1,12 @@
 require '../item'
 
 class MusicAlbum < Item
-    def initialize(on_spotify:, publish_date:)
-        super(publish_date)
-        @on_spotify = on_spotify
-    end
+  def initialize(on_spotify:, publish_date:)
+    super(publish_date)
+    @on_spotify = on_spotify
+  end
 
-    def can_be_archived?
-        super() && @on_spotify
-    end
+  def can_be_archived?
+    super() && @on_spotify
+  end
 end
