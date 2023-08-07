23794efb88f32cebd824e0d0f2a7bb83b8685470
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-08-07 23:07:29 +0500

    Catalogue: Adding source class

--- classes/movie/movie.rb
+++ classes/movie/movie.rb
@@ -1,10 +1,11 @@
 require_relative '../item_movie'
+require_relative '../source'
 
 class Movie < ItemMovie
   attr_accessor :archived
 
   def initialize(publish_date, id: nil, silent: true)
-    super(publish_date, id: id)
+    super(publish_date, id)
     @silent = silent
   end
 
@@ -17,3 +18,8 @@ movie = Movie.new('1999/02/28')
 puts movie.move_to_archive
 
 puts movie.archived
+
+source = Source.new('Motion Pictures')
+movie.add_source(source)
+puts movie.source.name
+puts source.items.first.publish_date
