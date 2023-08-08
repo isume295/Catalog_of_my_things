874d3e1deef7d05729ea47b2d88ee84b923f54d2
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-08 20:41:54 +0300

    Update music_spec.rb

--- rspec/music_spec.rb
+++ rspec/music_spec.rb
@@ -1,3 +1,20 @@
 require_relative '../classes/music/music_album'
 require 'rspec'
 
+describe MusicAlbum do
+  let(:publish_date) { '1999/02/28' }
+
+  subject { described_class.new(publish_date: publish_date, on_spotify: true) }
+
+  describe '#can_be_archived?' do
+    context 'when on_spotify is true' do
+      it 'returns true for a music released more than 10 years ago' do
+        expect(subject.can_be_archived?).to be(true)
+      end
+
+      it 'returns false for a music released less than 10 years ago' do
+        new_publish_date = '2023/02/28'
+        music_album = described_class.new(publish_date: new_publish_date, on_spotify: true)
+        expect(music_album.can_be_archived?).to be(false)
+      end
+    end
