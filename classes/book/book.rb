6579bdc5635f7919be391471670cdf13520cf3a6
Sumeya <sumeyaibrahim72@gmail.com>
2023-08-08 08:05:33 +0300

    Create book.rb

---
+++ classes/book/book.rb
@@ -0,0 +1,16 @@
+require_relative '../item_sume'
+
+class Book < Item
+    def initialize(cover_state, publisher, publish_date, id = nil)
+        super(publish_date,id)
+        @cover_state = cover_state
+        @publisher = publisher
+    end 
+
+    def can_be_archived
+        item_result = super
+        item_result || (@cover_state == "bad")
+    end
+
+end 
+
