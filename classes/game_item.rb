6b50f05294297aa21365bfaf613b5e28b6e7ed17
emmanuelsteven <write2stevemmy@gmail.com>
2023-08-08 11:28:50 +0100

    update game class & movie class

---
+++ classes/game_item.rb
@@ -0,0 +1,29 @@
+require 'date'
+
+class GameItem
+  def initialize(publish_date, id = nil)
+    @id = id || random.rand(1..1000)
+    @publish_date = publish_date
+  end
+
+  def add_genre; end
+
+  def add_author(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
+  def add_label; end
+
+  def add_source; end
+
+  def can_be_archived?
+    current_year = Date.today.year
+    publish_year = Date.parse(@publish_date).year
+    current_year - publish_year > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+end
