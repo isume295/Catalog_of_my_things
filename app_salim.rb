be401a10bb414dc18c6d56047b960b314b6cd1af
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-08 13:33:00 +0300

    Create app_salim.rb

---
+++ app_salim.rb
@@ -0,0 +1,6 @@
+require_relative 'classes/music/music_album'
+require_relative 'classes/music/genre'
+require_relative 'modules/music/addmusic'
+require_relative 'modules/music/displaymusic'
+require_relative 'modules/music/displaygenre'
+
