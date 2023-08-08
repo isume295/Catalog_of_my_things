6bf2475f0b441dd3c69ad462f61a945f65fdca4e
Sumeya <sumeyaibrahim72@gmail.com>
2023-08-08 08:05:39 +0300

    Create label.rb

---
+++ classes/book/label.rb
@@ -0,0 +1,14 @@
+class Label
+    attr_accessor :items, :color, :title, :id
+    def initialize(color, title, id = nil)
+        @id = id ||  Random.rand(1..1000)
+        @title = title
+        @items = []
+    end  
+
+    def add_item(item)
+        @items << item
+        item.label = self
+    end
+end
+
