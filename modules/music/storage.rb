8d666f3f015bcd19d1057d11f03fb0af6720074c
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-08 19:38:47 +0300

    Update storage.rb

--- modules/music/storage.rb
+++ modules/music/storage.rb
@@ -3,3 +3,9 @@ require 'pry'
     def save_file(file_name, data)
         File.write(file_name, JSON.pretty_generate(data))
     end
+
+    def save_music
+        save_music_albums
+        save_genres
+    end
+
