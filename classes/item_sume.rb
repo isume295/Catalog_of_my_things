bf4927cf7291148387cbce4cf01cf7aef275068f
Sumeya <sumeyaibrahim72@gmail.com>
2023-08-08 08:21:47 +0300

    Create item_sume.rb

---
+++ classes/item_sume.rb
@@ -0,0 +1,36 @@
+require 'date'
+require_relative 'book/label'
+
+class Item
+    attr_accessor :publish_date, :id, :label
+
+  def initialize(publish_date, id = nil)
+    @id = id || Random.rand(1..1000)
+    @publish_date = publish_date
+    @label = nil
+  end
+
+  def add_genre; end
+
+  def add_author; end
+
+  def add_label(label)
+    @label = label
+    @label.add_item(self)
+ end
+
+  def add_source; end
+
+  def can_be_archived?
+    current_year = Date.today.year
+    publish_year = Date.parse(@publish_date).year
+    current_year - publish_year > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+end
+
+# item = Item.new("2023-08-07")
+# puts item.can_be_archived?
