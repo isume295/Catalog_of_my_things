bc72ca9094e283862931c9f3f19f5b004de8c806
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-09 13:12:10 +0300

    Update genre_spec.rb

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,17 +1,17 @@
-# require_relative '../classes/music/genre'
-# require_relative '../classes/item_salim'
+require_relative '../classes/music/genre'
+require_relative '../classes/item'
 
-# describe Genre do
-#   let(:name) { 'Rock' }
+describe Genre do
+  let(:name) { 'Rock' }
 
-#   subject { described_class.new(name: name) }
+  subject { described_class.new(name: name) }
 
-#   describe '#add_item' do
-#     it 'adds an item to the genre and sets genre for the music album' do
-#       item = Item.new('2005/07/15')
-#       subject.add_item(item)
-#       expect(subject.items).to include(item)
-#       expect(item.genre).to eq(subject)
-#     end
-#   end
-# end
+  describe '#add_item' do
+    it 'adds an item to the genre and sets genre for the music album' do
+      item = Item.new('2005/07/15')
+      subject.add_item(item)
+      expect(subject.items).to include(item)
+      expect(item.genre).to eq(subject)
+    end
+  end
+end
