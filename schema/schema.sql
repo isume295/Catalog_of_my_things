1899226642bbafc9743af13835607ba869798f48
Salimer <52242629+Salimer@users.noreply.github.com>
2023-08-09 16:36:05 +0300

    Update schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -52,4 +52,24 @@ CREATE TABLE genre
         genre_id INT REFERENCES genre(id)
     );
 
-    
+    CREATE TABLE games (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  publish_date DATE,
+  author_id INT REFERENCES authors
+(id),
+  can_be_archived BOOLEAN,
+  multiplayer BOOLEAN,
+  last_played DATE,
+  PRIMARY KEY
+(id)
+);
+
+CREATE TABLE Authors (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ first_name VARCHAR
+(255),
+ last_name VARCHAR
+(255),
+ items TEXT
+ []
+);
