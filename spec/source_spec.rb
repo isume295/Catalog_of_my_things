7be43e6de3d1ec04ad9d8ffd4066e6a40614f396
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-08-09 13:40:44 +0500

    fixing

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -1,5 +1,5 @@
 require_relative '../classes/movie/source'
-require_relative '../classes/item_movie'
+require_relative '../classes/item'
 
 describe Source do
   let(:name) { 'Motion Pictures' }
@@ -8,7 +8,7 @@ describe Source do
 
   describe '#add_item' do
     it 'adds an item to the source and sets source for the item' do
-      item = ItemMovie.new('2005/07/15')
+      item = Item.new('2005/07/15')
       subject.add_item(item)
       expect(subject.items).to include(item)
       expect(item.source).to eq(subject)
